b5412464e5c6a3bb1ec0c1197bacbf4c6a902f8a
senghong01 <senghonghang@gmail.com>
2024-05-29 22:42:38 +0700

    [Config] add element-plus icons

--- stores/index.ts
+++ stores/index.ts
@@ -1,8 +1,20 @@
-import { defineStore } from "pinia";
+import { createPinia, defineStore } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
-export const useStore = defineStore('useStore', {
-    state: () => ({
-        name: 'hello world',
-    }),
+export const useStore = defineStore('app', {
+    state: () => {
+        return {
+            name: 'hello world',
+            locale: 'kh',
+        }
+    },
+    actions: {
+        changGlobalLanguage(lang: string) {
+            this.locale = 'en';
+           
+        }
+    },
     persist: true,
 })
